d67791495cd8833f524c01fd2a9394a7e76adfe8
Jungwook Kim <jw0905@naver.com>
2022-05-15 13:22:06 +0900

    gateway

---
+++ src/main/java/com/bitmoi/gateway/util/jwtUtil.java
@@ -0,0 +1,40 @@
+package com.bitmoi.gateway.util;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+public class jwtUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public Jwt validateToken(String token) {
+        SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+        return Jwts.parserBuilder().setSigningKey(key).build().parse(token);
+        // try {
+        // Jwts.parserBuilder().setSigningKey(key).build().parse(token);
+        // return true;
+        // } catch (ExpiredJwtException e) {
+        // throw new UnauthorizedException(ExceptionMessage.ExpiredToken.getMessage());
+        // } catch (MalformedJwtException e) {
+        // throw new
+        // UnauthorizedException(ExceptionMessage.MalformedToken.getMessage());
+        // } catch (SignatureException e) {
+        // throw new
+        // UnauthorizedException(ExceptionMessage.SignatureVerifyToken.getMessage());
+        // } catch (IllegalArgumentException e) {
+        // throw new
+        // UnauthorizedException(ExceptionMessage.IllegalArgumentToken.getMessage());
+        // } catch (Exception e) {
+        // throw new
+        // UnauthorizedException(ExceptionMessage.VerifyFailToken.getMessage());
+        // }
+    }
+}
