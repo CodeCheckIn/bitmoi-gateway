461f0f56c44bbe99c6c5e7dd166ce532d2a761db
Jungwook Kim <jw0905@naver.com>
2022-05-18 01:26:08 +0900

    filter add

--- src/main/java/com/bitmoi/gateway/filter/JwtAuthentication.java
+++ src/main/java/com/bitmoi/gateway/filter/JwtAuthentication.java
@@ -1,68 +1,58 @@
 package com.bitmoi.gateway.filter;
 
-import javax.crypto.SecretKey;
-
-import com.bitmoi.gateway.exception.jwtException;
+import java.nio.charset.StandardCharsets;
+import com.bitmoi.gateway.util.jwtUtil;
 import com.google.common.net.HttpHeaders;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthentication
         extends AbstractGatewayFilterFactory<JwtAuthentication.Config> {
 
-    // private final JwtProvider jwtProvider;
+    private final jwtUtil jwtUtils;
 
-    // public JwtAuthenticationGatewayFilterFactory(JwtProvider jwtProvider) {
-    // super(Config.class);
-    // this.jwtProvider = jwtProvider;
-    // }
-
-    // 토큰 검증을 할 로직을 apply 메서드에 추가하면 된다.
     @Override
     public GatewayFilter apply(Config config) {
 
-        // apply 메서드 안에서 첫 번째 파라미터는 ServerWebExchange 형태고, 두 번째 파라미터가 GatewayFilterChain
-        // 람다 함수이다.
         return ((exchange, chain) -> {
 
-            ServerHttpRequest request = exchange.getRequest(); // Request로 받아오면 Pre Filter가 적용된다. (주의 :
-                                                               // reactive.ServerHttpRequest 여야 한다.)
-
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
             // Request Header에 token이 존재하지 않을 경우
-            // if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-            // throw new
-            // UnauthorizedExceptio(ExceptionMessage.AuthVerifyAccessDenied.getMessage());
-            // }
+            if (!containsAuthorization(request)) {
+                return onError(response, "헤더가 존재하지 않습니다.", HttpStatus.BAD_REQUEST);
+            }
 
             // Request Header에서 token 추출
-            try {
-                String token = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-            } catch (Exception e) {
-                e.getMessage();
+
+            String token = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+            if (!jwtUtils.isValid(token)) {
+                return onError(response, "맞지 않은 토큰입니다.", HttpStatus.BAD_REQUEST);
             }
 
             return chain.filter(exchange);
-            // 토큰 검증 및 ADMIN 권한 확인
-            // if (jwtProvider.validateToken(token) &&
-            // jwtProvider.getMemberTypeFromToken(token).equals(MemberType.ADMIN.getName()))
-            // {
-            // return chain.filter(exchange).then(Mono.fromRunnable(() -> log.info("=====
-            // 관리자 토큰 검증 완료 =====")));
-            // } else {
-            // throw new
-            // UnauthorizedException(ExceptionMessage.AuthVerifyAccessDenied.getMessage());
-            // }
         });
+    }
+
+    private boolean containsAuthorization(ServerHttpRequest request) {
+        return request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION);
+    }
+
+    private Mono<Void> onError(ServerHttpResponse response, String message, HttpStatus status) {
+        response.setStatusCode(status);
+        DataBuffer buffer = response.bufferFactory().wrap(message.getBytes(StandardCharsets.UTF_8));
+        return response.writeWith(Mono.just(buffer));
     }
 
     // Config class에서는 Configuration 속성들을 넣어주면 되는데, Global Filter를 보자.
--- src/main/java/com/bitmoi/gateway/util/jwtUtil.java
+++ src/main/java/com/bitmoi/gateway/util/jwtUtil.java
@@ -1,40 +1,26 @@
 package com.bitmoi.gateway.util;
 
 import javax.crypto.SecretKey;
-
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
+@Component
 public class jwtUtil {
     @Value("${jwt.secret}")
     private String secret;
 
-    public Jwt validateToken(String token) throws Exception {
+    public boolean isValid(String token) {
         SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
-        // return Jwts.parserBuilder().setSigningKey(key).build().parse(token);
         try {
             Jwts.parserBuilder().setSigningKey(key).build().parse(token);
-            return Jwts.parserBuilder().setSigningKey(key).build().parse(token);
-        } catch (ExpiredJwtException e) {
-            throw new Exception(e.getMessage());
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
+            return true;
+        } catch (RuntimeException e) {
+            return false;
         }
-        // catch (SignatureException e) {
-        // throw new
-        // UnauthorizedException(ExceptionMessage.SignatureVerifyToken.getMessage());
-        // } catch (IllegalArgumentException e) {
-        // throw new
-        // UnauthorizedException(ExceptionMessage.IllegalArgumentToken.getMessage());
-        // } catch (Exception e) {
-        // throw new
-        // UnauthorizedException(ExceptionMessage.VerifyFailToken.getMessage());
-        // }
     }
+
 }
